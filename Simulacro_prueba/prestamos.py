30ef05be66a0117bb9f380b42423a5ff2d1bf293
echandia27 <jp.echandia@gmail.com>
2025-11-22 00:36:09 -0500

    avance en casa

--- Simulacro_prueba/prestamos.py
+++ Simulacro_prueba/prestamos.py
@@ -19,3 +19,65 @@ def cargar_prestamos():
 
 #guardar un prestamo nuevo
 
+def guardar_prestamo(nuevo):
+    campos=[
+        "prestamos id", "equipos_id", "nombre_equipo",
+        "usuario_prestatario", "tipo_usuario",
+        "fecha_solicitud", "fecha_prestamo", "fecha_devolucion",
+        "dias_autorizados", "dias_reales_usados",
+        "retraso", "estado", "mes", "anio"
+    ]
+
+    with open("prestamo.csv", mode="a", newline="",encoding="utf-8") as archivo:
+        escritor= csv.DictWriter(archivo, fieldnames=campos)
+
+        if archivo.tell == 0:
+            escritor.writeheader()
+
+        escritor.writerow(nuevo)
+
+def generar_id():
+    prestamos=cargar_prestamos()
+    if not prestamos:
+        return "P1"
+    
+    ultimo= prestamos[-1] [prestamos]
+    numero= int(ultimo[1:]) +1
+    return f"P{numero}"
+
+#registrar prestamo
+
+def registrar_prestamo():
+    print("\n=====Registrar Prestamo=====")
+    equipos= cargar_equipos()
+
+    #mostrar solo equipos disponibles
+    disponibles=[e for e in equipos if e['estado_actual']== "DISPONIBLE"]
+    #esto de abajo es lo mismo que lo de arriba
+    ''' disponibles = []
+    for e in equipos:
+    if e["estado_actual"] == "DISPONIBLE":  
+        disponibles.append(e)'''
+
+    if not disponibles:
+        print("\n No hay equipos disponibles")
+        return
+    print("\nEquipos disponibles:")
+    for e in disponibles:
+        print(f"- {e['equipo_id']} | {e['nombre equipo']} ({e['categoria']})")
+
+    equipo_id= input("\nIngrese id del equipo que desea prestar: ")
+
+    equipo= next((e for e in disponibles if e['equipo_id']== equipo_id), None)
+
+    if equipo is None:
+        print("\n No existe un equipo disponible con esa ID")
+        return
+    
+    #datos de prestatario
+    
+
+
+
+
+
