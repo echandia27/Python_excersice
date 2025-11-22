66a9ad53245907cd189c1a801ab910ba26eb17a1
echandia27 <jp.echandia@gmail.com>
2025-11-22 15:33:57 -0500

    avance

--- Simulacro_prueba/prestamos.py
+++ Simulacro_prueba/prestamos.py
@@ -75,7 +75,109 @@ def registrar_prestamo():
         return
     
     #datos de prestatario
+    try:
+        usuario= input("Nombre del usuario prestatario: ")
+        tipo= (input("tipo de usuario (ESTUDIANTE / INTRUCTOR / ADMINITRATIVO): ")).upper()
+    except ValueError:
+        print("ingresa un valor correcto")
+
+    if tipo not in ("ESTUDIANTE", "INTRUCTOR", "ADMINISTRATIVO"):
+        print("Tipo de Usuario invalido")
+        return
     
+    #fechas y dias
+    try:
+        fecha_prestamo= input("fecha del prestamo (YYYY-MM-DD): ")
+        dias= (input("Dias solicitados"))
+    except ValueError:
+        print("ingresa un valor correcto")
+
+    #limites
+
+    limites={"ESTUDIANTE": 3, "INTRUCTOR": 7, "ADMINISTRATIVO": 10 }
+
+    if dias > limites[tipo]:
+        print(f"Este usuario solo puede pedir {limites[tipo]} dias")
+        return
+    
+    hoy=datetime.today()
+    prestamo_id=generar_id()
+
+    nuevo_prestamo= {
+        "prestamo_id": prestamo_id,
+        "equipo_id": equipo['equipo_id'],
+        "nombre_equipo": equipo["nombre_equipo"],
+        "usuario_prestatario": usuario,
+        "tipo_usuario": tipo,
+        "fecha_solicitud": str(hoy.date()),
+        "fecha_prestamo": fecha_prestamo,
+        "fecha_devolucion": "",
+        "dias_autorizados": dias,
+        "dias_reales_usados": "",
+        "retraso": "",
+        "estado": "PENDIENTE",
+        "mes": hoy.month,
+        "anio": hoy.year
+    }
+
+    guardar_prestamo(nuevo_prestamo)
+
+    print("\n====Solicitud guardada como PENDIENTE\====n")
+
+    #listar prestamo
+
+def listar_prestamo():
+    print("\n----------Lista de prestamos-----------\n")
+
+    prestamos= cargar_prestamos()
+
+    if not prestamos:
+        print("\nNo hay prestanis registrados\n")
+        return
+    for p in prestamos:
+        print(f"{p['prestamo_id']} | Equipo: {p['nombre_equipo']} |"
+                f"{p['usuario_prestatario']} | Estado: {p['estado']}")
+            
+    #aprobar, rechazar prestamo
+
+def aprobar_prestamo():
+    prestamos= cargar_prestamos()
+    pendientes=[p for p in prestamos if p["estado"]== "PENDIENTE"]
+
+    if not pendientes:
+        print("\nNo hay solicitudes pendientes")
+        return
+    print("\nPrestamos Pendientes\n")
+    for p in pendientes:
+        print(f"{p['prestamo_id']}-{p['nombre_equipo']}")
+
+    try:
+        id_buscar= input("\nID del prestamo a aprobar/rechazar: ")
+    except ValueError:
+        print("Ingrese aprobar o rechazar")
+
+    for p in prestamos:
+        if p["prestamo_id"] == id_buscar:
+            decicion= input("\nAprobar unda (A) de lo contrario sera recahzado").upper()
+
+            if decicion =="A":
+                p["estado"]= "APROBADO"
+                print("\nPrestamo: APROBADO")
+            else:
+                p["estado"]= "RECHAZADO"
+                print("\nPrestamo: RECHAZADO")
+
+            #guardar cambios    
+            sobreescribir_prestamos(prestamos)
+            return
+    print("\nNo se encontro ID\n")
+
+#sobre escribir archivo con prestamo actualizado
+
+def sobreescribir_prestamos(lista):
+
+
+
 
 
 
